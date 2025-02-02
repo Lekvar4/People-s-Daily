c182b0378e56c15e1f7dccfeffb2eb967f0e3a4e
Lekvar4 <ajiboyelateef940@gmail.com>
2025-02-02 17:15:54 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -64,3 +64,11 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.menu-icon').addEventListener('click', function() {
   document.querySelector('.nav-links').classList.toggle('active');
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.getElementById("menu-icon");
+    const navLinks = document.getElementById("nav-links");
+
+    menuIcon.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+    });
+});
